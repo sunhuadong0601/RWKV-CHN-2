153baabbfdac02059954971178f0ae204bf3dda2
BlinkDL <blinkdl@163.com>
2023-04-05 14:02:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ os.environ["RWKV_JIT_ON"] = '1'
 os.environ["RWKV_CUDA_ON"] = '1' # if '1' then use CUDA kernel for seq mode (much faster)
 
 from rwkv.model import RWKV
-model_path = hf_hub_download(repo_id="BlinkDL/rwkv-4-pile-7b", filename="RWKV-4-Pile-7B-Chn-testNovel-664-ctx2048-20230327.pth")
+model_path = hf_hub_download(repo_id="BlinkDL/rwkv-4-pile-7b", filename="RWKV-4-Pile-7B-Chn-testNovel-done-ctx2048-20230404.pth")
 model = RWKV(model=model_path, strategy='cuda fp16i8 *12 -> cuda fp16')
 from rwkv.utils import PIPELINE, PIPELINE_ARGS
 pipeline = PIPELINE(model, "20B_tokenizer.json")
